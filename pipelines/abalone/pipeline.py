be3cf4a2e4691f6e60ce13559a245927b0758045
Raquel Peña <69599122+kawaiiblitz@users.noreply.github.com>
2025-05-25 16:15:33 -0600

    Update pipeline.py

--- pipelines/abalone/pipeline.py
+++ pipelines/abalone/pipeline.py
@@ -1,2 +1,11 @@
+from sagemaker.workflow.pipeline import Pipeline
+from sagemaker.workflow.parameters import ParameterString
+
 def get_pipeline():
-    print("Mock pipeline created")
+    return Pipeline(
+        name="abalone-pipeline",
+        parameters=[
+            ParameterString(name="InputDataUrl", default_value="s3://dummy-url/")
+        ],
+        steps=[]
+    )
